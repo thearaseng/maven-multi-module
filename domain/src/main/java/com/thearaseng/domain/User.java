854d4fa4c49ca3b6c51ecad8be3474c782e81770
Theara Seng <seng.theara.kh@gmail.com>
2018-05-06 13:59:14 +0700

    add User model

---
+++ domain/src/main/java/com/thearaseng/domain/User.java
@@ -0,0 +1,28 @@
+package com.thearaseng.domain;
+
+/**
+ * @author Theara Seng
+ * created on May 06, 2018
+ */
+
+public class User {
+
+    private Long id;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
